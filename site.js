2079eba99c1cf8a8b6935100ba1c7e74d6edaf41
SummerEcho <164468785+NeebinNodin@users.noreply.github.com>
2025-02-05 01:18:55 -0600

    Dynamic stuff added

--- site.js
+++ site.js
@@ -1,8 +1,33 @@
 
+alert('Welcome to my website!')
 
+// Added for Assignment 3. Alot of experimenting with different parameters/values/properties/or whatever the proper term is.
+document.addEventListener('DOMContentLoaded', function() {
+
+const hours = new Date().getHours()
+
+const isMorning = hours >= 4 && hours <12
+const isAfternoon = hours >= 12 && hours < 17
+const isEvening = hours >= 17 || hours < 4
+
+let message = ''
+
+if (isMorning) {
+    message = 'Good Morning!'
+} else if (isAfternoon) {
+    message = 'Good Afternoon!'
+} else if (isEvening) {
+    message = 'Good Evening!'
+}
+
+const welcome = document.getElementById('welcome')
+welcome.textContent = message
+
+
+///// Put this at the end. Seemed to be the better placement since it is last in the code. Or might have errors if placed elsewhere.
 const h2 = document.querySelector('h2')
 setTimeout(() => {
-    h2.textContent = 'and Salutations'
+    h2.textContent = welcome.textContent
 }, 3000)
 
-alert('Welcome to my website!')
+})// Placed const h2 inside of the DOMContentLoaded since it is the last thing to be executed and didn't think of it until I ask CoPilot some general questions. Had it outside. 
